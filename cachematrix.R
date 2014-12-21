26e26db065872f5c4dc1b257609dc0d447c24324
Jeisar <jassonstef@gmail.com>
2014-12-21 20:16:34 +0200

    created assignment 2 code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cachematrix.R
+## This function calculates the inverse of a matrix, while uses caching to avoid unecessary computing time. 
+## If the inverse matrix is already calculated it is simply retrieved from cache, avoiding the re-calculation.
+## It is assumed that the input matrix is always invertible.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## The makeCacheMatrix function received the input matrix and creates a special list that includes the four 
+## functions required to do the calculations.
+makeCacheMatrix <- function(x = numeric()) {
+    inv<-NULL                                                    
+    set<-function(y)
+        x  <<-y                                                   
+        inv<<-NULL       
+    
+    get<- function() x
+    setinv <- function(inverse) inv <<-inverse
+    getinv <- function() inv                                
+    list(set=set, get = get, setinv = setinv, getinv = getinv)   
 }
 
 
-## Write a short comment describing this function
-
+## The cacheSolve function uses the special list of functions from the makeCacheMatrix to actually perform the 
+## inverse calculation or cache retrieval.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinv()
+    ## If the inverse matrix from cache is not zero then the calculations are completed and the retrieved-from-cache inverse matrix is shown on screen.
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    ## If the inverse matrix from cache is zero, then the inverse matrix of the input matrix is calculated using the solve() function.
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$setinv(inv)
+    inv
+    
+    
+    
 }
+# source("cachematrix.R") paste this in the promt to load the functions.
+
